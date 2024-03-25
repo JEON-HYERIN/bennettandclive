81d45efe92a560e1fd4ce5ffe68bfd7f7c4efc36
jeonhyelin <jeonhyelin19951118@gmail.com>
2024-03-25 23:49:27 +0900

    Fix: partner 마크업

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,6 +33,17 @@ document.querySelectorAll('.section-home__video').forEach((element, index) => {
   )
 });
 
+$(window).on('scroll', function() {
+  const target = $('.section-about').offset().top;
+  const scroll = $(window).scrollTop();
+  if(scroll >= target) {
+    $('.header').addClass('is-blended');
+  } else {
+    $('.header').removeClass('is-blended');
+  }
+})
+
+
 ScrollTrigger.create({
   trigger: '.section-home',
   start: '100% center',
@@ -49,3 +60,79 @@ ScrollTrigger.create({
     $('.floating-logo').addClass('is-blended');
   }
 })
+
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.section-home',
+//     start: '0% 0%',
+//     end: '100% 100%',
+//     scrub: 0
+//   }
+// })
+
+// document.querySelectorAll('.section-service__list--right .section-service__item').forEach(function(el, index) {
+//   tl.to($('.section-service__list--right .section-service__item').eq(index).find('.section-service__text'), {
+//     scale: 0
+//   })
+// })
+
+// ScrollTrigger.create({
+//   trigger: '.section-service',
+//   start: '0% 0%',
+//   end: '100% 0',
+//   markers: true,
+//   onLeaveBack: function() {
+//     imgEl = $('.section-service__list--right .section-service__item');
+//     total = imgEl.length;
+//     // $('.section-service__list--right .section-service__item').eq(total).addClass('on');
+//   },
+//   onUpdate: function(self) {
+
+//     imgEl = $('.section-service__list--right .section-service__item');
+//     total = imgEl.length;
+//     currImg = Math.round((self.progress * total));
+//     curr = imgEl.eq(currImg).find('.section-service__text');
+//     console.log(currImg);
+
+//     gsap.to(curr, {
+//       scale: 0,
+//       duration: .2,
+//     })
+
+
+//     if($('.section-service__list--right .section-service__item.on')) {
+//       // imgEl.removeClass('on');
+//     }
+
+//     if(curr) {
+//       // curr.addClass('on');
+//     }
+//     // console.log(Math.round(self.progress * imgLength));
+//   }
+// })
+
+// const reveal = gsap.utils.toArray('.section-service__list--right .section-service__item');
+//   reveal.forEach((text,i)=>{
+//     ScrollTrigger.create({
+//      trigger:text,
+//     //  toggleClass:'active',
+//     //  start:"top 90%",
+//     //  end:"top 20%",
+//       start: '0% 0%',
+//       // end: '50% 0',
+//      markers:true,
+//      scrub:true,
+//      onUpdate: function(self) {
+//       gsap.fromTo($('.section-service__list--right .section-service__item').eq(i).find('.section-service__text'), {
+//         scale: 1,
+//       }, {
+//         scale: 0,
+//         duration: Math.round(self.progress),
+//       })
+//       console.log(i)
+//      }
+//   });
+// });
+
+// const tl = gsap.timeline({});
+// tl.fromTo('')
