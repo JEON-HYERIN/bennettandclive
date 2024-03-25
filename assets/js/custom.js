428e6155c92624b6c32388530d3b090519e3128c
jeonhyelin <jeonhyelin19951118@gmail.com>
2024-03-25 18:20:22 +0900

    Fix: home 영역 마크업

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,51 +24,57 @@ bar = gsap.timeline({
   }
 })
 
-document.querySelectorAll('.section-home__slide').forEach((element, index) => {
-  bar.to($('.section-home__slide').eq(index), {
-    ease: "none",
-    onComplete: function () {
-      stickySlide.slideTo(index + 1)
-    },
-    onReverseComplete: function () {
-      stickySlide.slideTo(index - 1)
+// document.querySelectorAll('.section-home__video').forEach((element, index) => {
+//   bar.to($('.section-home__video').eq(index), {
+//     ease: "none",
+//     onComplete: function () {
+//       stickySlide.slideTo(index + 1)
+//     },
+//     onReverseComplete: function () {
+//       stickySlide.slideTo(index - 1)
+//     }
+//   })
+// });
+document.querySelectorAll('.section-home__video').forEach((element, index) => {
+  bar.fromTo($('.section-home__video').eq(index + 1), {
+    yPercent: 100,
+  }, {
+    yPercent: 0,
+    scrub: 3,
+    ease: 'none',
+    duration: 15,
+    onComplete: function() {
+      // $('.section-home__video').eq(index + 1).addClass('is-visible').siblings().removeClass('is-visible');
     }
-  })
+  }
+  )
 });
 
-ScrollTrigger.create({
-  trigger: '.section-home .first',
-  start: '0% 0%',
-  end: '100% 0',
-  // markers: true,
-  onLeaveBack: function() {
-    imgEl = $('.section-home__title');
-    total = imgEl.length;
-    $('.first .section-home__title').eq(total).addClass('on');
-  },
-  onUpdate: function(self) {
+// ScrollTrigger.create({
+//   trigger: '.section-home .first',
+//   start: '0% 0%',
+//   end: '100% 0',
+//   // markers: true,
+//   onLeaveBack: function () {
+//     imgEl = $('.section-home__title');
+//     total = imgEl.length;
+//     $('.first .section-home__title').eq(total).addClass('on');
+//   },
+//   onUpdate: function (self) {
 
-    imgEl = $('.first .section-home__title');
-    total = imgEl.length;
-    currImg = Math.round(self.progress * total);
-    curr = imgEl.eq(currImg);
+//     imgEl = $('.first .section-home__title');
+//     total = imgEl.length;
+//     currImg = Math.round(self.progress * total);
+//     curr = imgEl.eq(currImg);
 
-    if($('.first .section-home__title.on')) {
-      imgEl.removeClass('on');
-    }
-    console.log(currImg, self.progress)
+//     if ($('.first .section-home__title.on')) {
+//       imgEl.removeClass('on');
+//     }
+//     console.log(currImg, self.progress)
 
-    if(curr) {
-      curr.addClass('on');
-    }
-    // console.log(Math.round(self.progress * imgLength));
-  }
-})
-
-$('.nav__link').on('mouseenter', function() {
-  // $(this).parent().addClass('is-active').siblings().removeClass('is-active');
-})
-
-$('.nav__link').on('mouseleave', function() {
-  // $('.nav__item').eq(0).addClass('is-active').siblings().removeClass('is-active');
-})
+//     if (curr) {
+//       curr.addClass('on');
+//     }
+//     // console.log(Math.round(self.progress * imgLength));
+//   }
+// })
